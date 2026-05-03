15f41e18cc247f5e457edac723c8ac235bf55713
Dwip Nandi <dwipnandi100@gmail.com>
2026-05-03 17:58:48 +0600

    table retrieve

--- a1.sql
+++ a1.sql
@@ -5,3 +5,13 @@ SELECT NAME FROM SYSOBJECTS WHERE XTYPE ='U' --- seen the all tables of the data
 SELECT name,id FROM SYSOBJECTS WHERE XTYPE ='U' --- seen the all tables of the database  -- show only "name"and "id" columns
 
 ------------2----------
+SELECT * FROM <TABLENAME> -- show all column in a table
+SELECT column_name FROM <TABLENAME> -- show addressing column in a table
+SELECT <COLUMNNAME1>, <COLUMNNAME2> FROM AUTHORS --- if want to see multiple column
+SELECT * FROM AUTHORS WHERE <COLUMNNAME> <CONDITION>  -- seen the my conditionaly
+SELECT * from authors where state='CA' -----show data from the state of CA
+SELECT * from authors where state='CA' and city='Oakland' ----execute multiple condition
+-- task1---> 
+SELECT title from titles Where ytd_sales>8000
+--task2--->
+SELECT title from titles where royalty>=12 and royalty<=24
