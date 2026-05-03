b87f388e04e768547d7aee6c282f76a6136c98b1
Dwip Nandi <dwipnandi100@gmail.com>
2026-05-03 23:13:43 +0600

    Cartesian Product

--- a2.sql
+++ a2.sql
@@ -12,3 +12,10 @@ FROM titles
 INNER JOIN titleauthor ON titles.title_id = titleauthor.title_id
 INNER JOIN authors ON titleauthor.au_id = authors.au_id
 INNER JOIN publishers ON titles.pub_id = publishers.pub_id;
+
+----------------2----------------
+--task2-----
+--iii
+SELECT authors.au_fname, authors.au_lname, authors.city, publishers.pub_name, publishers.city
+FROM authors, publishers
+WHERE authors.city = publishers.city;
