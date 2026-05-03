9cfce9752684205f2f6265ed44d1be8795d8744e
Dwip Nandi <dwipnandi100@gmail.com>
2026-05-03 22:59:01 +0600

    upadete

--- a2.sql
+++ a2.sql
@@ -1,11 +1,12 @@
 ---------------1--------------
 --task1-----
+--i
 SELECT titles.title, authors.au_fname, authors.au_lname 
 FROM titles 
 INNER JOIN titleauthor ON titles.title_id = titleauthor.title_id
 INNER JOIN authors ON titleauthor.au_id = authors.au_id;
 
---task2-------
+--ii
 SELECT titles.title, authors.au_fname, authors.au_lname, publishers.pub_name
 FROM titles
 INNER JOIN titleauthor ON titles.title_id = titleauthor.title_id
